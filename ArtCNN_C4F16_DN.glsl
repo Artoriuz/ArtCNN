842ef26e6cdf4f9b19ac427212861a5492ec80a0
Joao Chrisostomo <joao00999@gmail.com>
2024-01-30 12:40:52 -0300

    minor fixes (not sure how I fucked this up)

--- ArtCNN_C4F16_DN.glsl
+++ ArtCNN_C4F16_DN.glsl
@@ -20,7 +20,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-//!DESC C4F16 DN (Conv-0)
+//!DESC ArtCNN C4F16 DN (Conv-0)
 //!HOOK LUMA
 //!BIND LUMA
 //!SAVE conv2d_tf
@@ -43,7 +43,7 @@ vec4 hook() {
     return result;
 }
 
-//!DESC C4F16 DN (Conv-0)
+//!DESC ArtCNN C4F16 DN (Conv-0)
 //!HOOK LUMA
 //!BIND LUMA
 //!SAVE conv2d_tf1
@@ -66,7 +66,7 @@ vec4 hook() {
     return result;
 }
 
-//!DESC C4F16 DN (Conv-0)
+//!DESC ArtCNN C4F16 DN (Conv-0)
 //!HOOK LUMA
 //!BIND LUMA
 //!SAVE conv2d_tf2
@@ -89,7 +89,7 @@ vec4 hook() {
     return result;
 }
 
-//!DESC C4F16 DN (Conv-0)
+//!DESC ArtCNN C4F16 DN (Conv-0)
 //!HOOK LUMA
 //!BIND LUMA
 //!SAVE conv2d_tf3
@@ -112,7 +112,7 @@ vec4 hook() {
     return result;
 }
 
-//!DESC C4F16 DN (Conv-1-ReLU)
+//!DESC ArtCNN C4F16 DN (Conv-1-ReLU)
 //!HOOK LUMA
 //!BIND conv2d_tf
 //!BIND conv2d_tf1
@@ -168,7 +168,7 @@ vec4 hook() {
     return max(result, vec4(0.0));
 }
 
-//!DESC C4F16 DN (Conv-1-ReLU)
+//!DESC ArtCNN C4F16 DN (Conv-1-ReLU)
 //!HOOK LUMA
 //!BIND conv2d_tf
 //!BIND conv2d_tf1
@@ -224,7 +224,7 @@ vec4 hook() {
     return max(result, vec4(0.0));
 }
 
-//!DESC C4F16 DN (Conv-1-ReLU)
+//!DESC ArtCNN C4F16 DN (Conv-1-ReLU)
 //!HOOK LUMA
 //!BIND conv2d_tf
 //!BIND conv2d_tf1
@@ -280,7 +280,7 @@ vec4 hook() {
     return max(result, vec4(0.0));
 }
 
-//!DESC C4F16 DN (Conv-1-ReLU)
+//!DESC ArtCNN C4F16 DN (Conv-1-ReLU)
 //!HOOK LUMA
 //!BIND conv2d_tf
 //!BIND conv2d_tf1
@@ -336,7 +336,7 @@ vec4 hook() {
     return max(result, vec4(0.0));
 }
 
-//!DESC C4F16 DN (Conv-2-ReLU)
+//!DESC ArtCNN C4F16 DN (Conv-2-ReLU)
 //!HOOK LUMA
 //!BIND conv2d_1_tf
 //!BIND conv2d_1_tf1
@@ -392,7 +392,7 @@ vec4 hook() {
     return max(result, vec4(0.0));
 }
 
-//!DESC C4F16 DN (Conv-2-ReLU)
+//!DESC ArtCNN C4F16 DN (Conv-2-ReLU)
 //!HOOK LUMA
 //!BIND conv2d_1_tf
 //!BIND conv2d_1_tf1
@@ -448,7 +448,7 @@ vec4 hook() {
     return max(result, vec4(0.0));
 }
 
-//!DESC C4F16 DN (Conv-2-ReLU)
+//!DESC ArtCNN C4F16 DN (Conv-2-ReLU)
 //!HOOK LUMA
 //!BIND conv2d_1_tf
 //!BIND conv2d_1_tf1
@@ -504,7 +504,7 @@ vec4 hook() {
     return max(result, vec4(0.0));
 }
 
-//!DESC C4F16 DN (Conv-2-ReLU)
+//!DESC ArtCNN C4F16 DN (Conv-2-ReLU)
 //!HOOK LUMA
 //!BIND conv2d_1_tf
 //!BIND conv2d_1_tf1
@@ -560,7 +560,7 @@ vec4 hook() {
     return max(result, vec4(0.0));
 }
 
-//!DESC C4F16 DN (Conv-3-ReLU)
+//!DESC ArtCNN C4F16 DN (Conv-3-ReLU)
 //!HOOK LUMA
 //!BIND conv2d_2_tf
 //!BIND conv2d_2_tf1
@@ -616,7 +616,7 @@ vec4 hook() {
     return max(result, vec4(0.0));
 }
 
-//!DESC C4F16 DN (Conv-3-ReLU)
+//!DESC ArtCNN C4F16 DN (Conv-3-ReLU)
 //!HOOK LUMA
 //!BIND conv2d_2_tf
 //!BIND conv2d_2_tf1
@@ -672,7 +672,7 @@ vec4 hook() {
     return max(result, vec4(0.0));
 }
 
-//!DESC C4F16 DN (Conv-3-ReLU)
+//!DESC ArtCNN C4F16 DN (Conv-3-ReLU)
 //!HOOK LUMA
 //!BIND conv2d_2_tf
 //!BIND conv2d_2_tf1
@@ -728,7 +728,7 @@ vec4 hook() {
     return max(result, vec4(0.0));
 }
 
-//!DESC C4F16 DN (Conv-3-ReLU)
+//!DESC ArtCNN C4F16 DN (Conv-3-ReLU)
 //!HOOK LUMA
 //!BIND conv2d_2_tf
 //!BIND conv2d_2_tf1
@@ -784,7 +784,7 @@ vec4 hook() {
     return max(result, vec4(0.0));
 }
 
-//!DESC C4F16 DN (Conv-4-ReLU)
+//!DESC ArtCNN C4F16 DN (Conv-4-ReLU)
 //!HOOK LUMA
 //!BIND conv2d_3_tf
 //!BIND conv2d_3_tf1
@@ -840,7 +840,7 @@ vec4 hook() {
     return max(result, vec4(0.0));
 }
 
-//!DESC C4F16 DN (Conv-4-ReLU)
+//!DESC ArtCNN C4F16 DN (Conv-4-ReLU)
 //!HOOK LUMA
 //!BIND conv2d_3_tf
 //!BIND conv2d_3_tf1
@@ -896,7 +896,7 @@ vec4 hook() {
     return max(result, vec4(0.0));
 }
 
-//!DESC C4F16 DN (Conv-4-ReLU)
+//!DESC ArtCNN C4F16 DN (Conv-4-ReLU)
 //!HOOK LUMA
 //!BIND conv2d_3_tf
 //!BIND conv2d_3_tf1
@@ -952,7 +952,7 @@ vec4 hook() {
     return max(result, vec4(0.0));
 }
 
-//!DESC C4F16 DN (Conv-4-ReLU)
+//!DESC ArtCNN C4F16 DN (Conv-4-ReLU)
 //!HOOK LUMA
 //!BIND conv2d_3_tf
 //!BIND conv2d_3_tf1
@@ -1008,7 +1008,7 @@ vec4 hook() {
     return max(result, vec4(0.0));
 }
 
-//!DESC C4F16 DN (Conv-5)
+//!DESC ArtCNN C4F16 DN (Conv-5)
 //!HOOK LUMA
 //!BIND conv2d_4_tf
 //!BIND conv2d_4_tf1
@@ -1064,7 +1064,7 @@ vec4 hook() {
     return result;
 }
 
-//!DESC C4F16 DN (Conv-5)
+//!DESC ArtCNN C4F16 DN (Conv-5)
 //!HOOK LUMA
 //!BIND conv2d_4_tf
 //!BIND conv2d_4_tf1
@@ -1120,7 +1120,7 @@ vec4 hook() {
     return result;
 }
 
-//!DESC C4F16 DN (Conv-5)
+//!DESC ArtCNN C4F16 DN (Conv-5)
 //!HOOK LUMA
 //!BIND conv2d_4_tf
 //!BIND conv2d_4_tf1
@@ -1176,7 +1176,7 @@ vec4 hook() {
     return result;
 }
 
-//!DESC C4F16 DN (Conv-5)
+//!DESC ArtCNN C4F16 DN (Conv-5)
 //!HOOK LUMA
 //!BIND conv2d_4_tf
 //!BIND conv2d_4_tf1
@@ -1232,7 +1232,7 @@ vec4 hook() {
     return result;
 }
 
-//!DESC C4F16 DN (Long-Skip-Conv-6)
+//!DESC ArtCNN C4F16 DN (Long-Skip-Conv-6)
 //!HOOK LUMA
 //!BIND conv2d_5_tf
 //!BIND conv2d_tf
