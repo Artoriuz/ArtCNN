a980a1e0ad54f5c5556b78d5736f24a1066af4a1
Joao Chrisostomo <joao00999@gmail.com>
2025-07-19 14:27:18 -0300

    Removing unused scripts

--- Scripts/benchmarker.py
+++ Scripts/benchmarker.py
@@ -1,31 +1,21 @@
-from scipy.ndimage import convolve
-import numpy as np
+import jax.numpy as jnp
+import dm_pix as pix
+import jax
 import cv2
 import glob
-import skimage
 
-def msssim(im1, im2, data_range = 255, channel_axis = None):
+def msssim(y_true, y_pred, max_val = 1):
     level = 5
-    weights = np.array([0.0448, 0.2856, 0.3001, 0.2363, 0.1333])
-    downsample_filter = np.ones((2, 2))/4.0
+    weights = jnp.array([0.0448, 0.2856, 0.3001, 0.2363, 0.1333])
     msssim = []
     for _ in range(level):
-        ssim_res = skimage.metrics.structural_similarity(im1 = im1, im2 = im2, data_range = data_range, channel_axis = channel_axis)
+        ssim_res = pix.ssim(y_true, y_pred, max_val = max_val)
         msssim.append(ssim_res)
-        if channel_axis:
-            filtered_im1 = np.zeros_like(im1)
-            filtered_im2 = np.zeros_like(im2)
-            for channel in range(channel_axis):
-                filtered_im1[:, :, channel] = convolve(im1[:, :, channel], downsample_filter, mode='reflect')
-                filtered_im2[:, :, channel] = convolve(im2[:, :, channel], downsample_filter, mode='reflect')
-            im1 = filtered_im1[::2, ::2, :]
-            im2 = filtered_im2[::2, ::2, :]
-        else:    
-            filtered_im1 = convolve(im1, downsample_filter, mode='reflect')
-            filtered_im2 = convolve(im2, downsample_filter, mode='reflect')
-            im1 = filtered_im1[::2, ::2]
-            im2 = filtered_im2[::2, ::2]
-    return np.average(np.array(msssim), weights=weights)
+        b, h, w, c = y_true.shape
+        y_true = jax.image.resize(y_true, (b, h // 2, w // 2, c), method="bilinear")
+        y_pred = jax.image.resize(y_pred, (b, h // 2, w // 2, c), method="bilinear")
+    msssim = jnp.sum(jnp.stack(msssim, axis=1) * weights, axis=1)
+    return msssim
 
 output_file = open("benchmark_result.txt", "w")
 
@@ -33,18 +23,24 @@ print("Starting benchmarks")
 
 reference = cv2.imread('./reference.png', cv2.IMREAD_COLOR)
 reference = cv2.cvtColor(reference, cv2.COLOR_BGR2GRAY, 0).astype(float) / 255.0
+reference = jnp.asarray(reference)
+reference = jnp.expand_dims(reference, 0)
+reference = jnp.expand_dims(reference, -1)
 
 filelist = sorted(glob.glob('./*.png'))
 for myFile in filelist:
     if not "downscaled" in myFile:
         image = cv2.imread(myFile, cv2.IMREAD_COLOR)
         image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY, 0).astype(float) / 255.0
+        image = jnp.asarray(image)
+        image = jnp.expand_dims(image, 0)
+        image = jnp.expand_dims(image, -1)
 
-        mae_score = np.mean(np.absolute(reference - image))
-        psnr_score = skimage.metrics.peak_signal_noise_ratio(reference, image)
-        ssim_score = skimage.metrics.structural_similarity(reference, image, data_range = 1, channel_axis = None)
-        msssim_score = msssim(reference, image, data_range = 1, channel_axis = None)
-        
+        mae_score = pix.mae(reference, image)
+        psnr_score = pix.psnr(reference, image)
+        ssim_score = pix.ssim(reference, image, max_val = 1)
+        msssim_score = msssim(reference, image, max_val = 1)
+
         print(f"{myFile} - MAE: {mae_score}, PSNR: {psnr_score}, SSIM: {ssim_score}, MS-SSIM: {msssim_score}\n")
         output_file.write(f"{myFile}, {mae_score}, {psnr_score}, {ssim_score}, {msssim_score}\n")
 
